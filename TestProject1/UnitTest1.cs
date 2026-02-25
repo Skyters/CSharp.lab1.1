11b252f2c98bfce116efa79dd5fc53b85a47d023
Skyters <vlad.307123@gmail.com>
2026-02-25 15:46:23 +0800

    Были созданы и проведены тесты.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using lab1._1;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var result = Logic.Result(20);
+            Assert.That(result, Is.EqualTo("0 рублей 20 копеек"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+            var result = Logic.Result(500);
+            Assert.That(result, Is.EqualTo("5 рублей 0 копеек"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+
+            var result = Logic.Result(250);
+            Assert.That(result, Is.EqualTo("2 рубля 50 копеек"));
+        }
+    }
+}

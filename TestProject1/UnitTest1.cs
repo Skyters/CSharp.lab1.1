f6b8ab00be27896689062807500a9bd85bc6f6e9
Skyters <vlad.307123@gmail.com>
2026-03-11 12:39:07 +0800

    Добавлены тесты

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -15,13 +15,43 @@ namespace TestProject1
         [Test]
         public void TestConverting2()
         {
+            var result = Logic.ConversionPennyStringToRubles(21);
+            Assert.That(result, Is.EqualTo("21 копейка"));
+        }
+
+        [Test]
+        public void TestConverting3()
+        {
+            var result = Logic.ConversionPennyStringToRubles(23);
+            Assert.That(result, Is.EqualTo("23 копейки"));
+        }
+
+        [Test]
+        public void TestConverting4()
+        {
 
             var result = Logic.ConversionPennyStringToRubles(500);
             Assert.That(result, Is.EqualTo("5 рублей "));
         }
 
         [Test]
-        public void TestConverting3()
+        public void TestConverting5()
+        {
+
+            var result = Logic.ConversionPennyStringToRubles(100);
+            Assert.That(result, Is.EqualTo("1 рубль "));
+        }
+
+        [Test]
+        public void TestConverting6()
+        {
+
+            var result = Logic.ConversionPennyStringToRubles(2300);
+            Assert.That(result, Is.EqualTo("23 рубля "));
+        }
+
+        [Test]
+        public void TestConverting7()
         {
 
             var result = Logic.ConversionPennyStringToRubles(250);
